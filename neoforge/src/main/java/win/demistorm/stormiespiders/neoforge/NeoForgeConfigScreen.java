70aa0264e55fb975fc3a7a1df677a0ee70bd72ba
halfstorm <halfstorm@nexigate.win>
2025-11-15 11:07:55 -0600

    Fixed NeoForge build, spiders still not spawning

--- neoforge/src/main/java/win/demistorm/stormiespiders/neoforge/NeoForgeConfigScreen.java
+++ neoforge/src/main/java/win/demistorm/stormiespiders/neoforge/NeoForgeConfigScreen.java
@@ -1,16 +1,13 @@
 package win.demistorm.stormiespiders.neoforge;
 
-import net.minecraft.client.gui.screens.Screen;
 import net.neoforged.fml.ModLoadingContext;
-import net.neoforged.neoforge.client.ConfigScreenHandler;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import win.demistorm.stormiespiders.client.config.ConfigScreen;
 
 public class NeoForgeConfigScreen {
 
     public static void register() {
-        // Register config screen
-        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
-            () -> new ConfigScreenHandler.ConfigScreenFactory((minecraft, screen) ->
-                ConfigScreen.SimpleToggleScreen.create(screen)));
+        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class,
+                () -> (minecraft, screen) -> ConfigScreen.SimpleToggleScreen.create(screen));
     }
 }
