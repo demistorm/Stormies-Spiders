20ccbfe726d1451f3b0b567f4b5213f51665b1d0
halfstorm <halfstorm@nexigate.win>
2026-06-13 18:51:35 -0500

    Fix clumping issues

--- common/src/main/java/win/demistorm/stormiespiders/mixin/ClimberEntityMixin.java
+++ common/src/main/java/win/demistorm/stormiespiders/mixin/ClimberEntityMixin.java
@@ -577,10 +577,7 @@ public abstract class ClimberEntityMixin extends PathfinderMob implements IClimb
 
 	@Override
 	public boolean onClimbable() {
-		if (RotationOverrideConfig.isClimberDisabled(this.getType())) {
-			return this.horizontalCollision;
-		}
-		return true;
+		return this.horizontalCollision;
 	}
 
 	@Override
